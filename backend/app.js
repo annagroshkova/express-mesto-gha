3ffe6aea66010ae3ce80b318723b46a11df5fb1b
Anna Groshkova <anna.matvyeyenko@gmail.com>
2023-06-01 23:24:44 +0200

    fix

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Max-Age: 86400');
+    res.header('Access-Control-Max-Age', '86400');
     return res.end();
   }
 
